756d4679a96034adf30f97284b8beea55fb1a651
Talha Azhar <your.email@example.com>
2024-08-23 21:16:57 +0500

    Normalize line endings

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import NavBar from './Component/NavBar/nav';
+import Footer from './Component/Footer/footer';
+import Adoption from './Component/Adoption/adoption';
+
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      <Adoption/>
+      <Footer/>
     </div>
   );
 }
