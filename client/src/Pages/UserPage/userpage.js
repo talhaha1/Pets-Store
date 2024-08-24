5d95b266460d091a178d475a4eaf8f40a3f8fdc8
Talha Azhar <your.email@example.com>
2024-08-24 20:05:20 +0500

    Front end updated with some changes, form and other adoption using jwt was updated

--- client/src/Pages/UserPage/userpage.js
+++ client/src/Pages/UserPage/userpage.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Nav from '../../Components/NavBar/nav';
 import Footer from '../../Components/Footer/footer';
-import Inventory from '../../Components/Inventory/Inventory';
 import AdoptPets from '../../Components/AdoptPets/adoption';
+
 const AdminPage=()=>{
     return(
         <div>
             <Nav/>
-            <Inventory/>
             <AdoptPets/>
             <Footer/>
         </div>
