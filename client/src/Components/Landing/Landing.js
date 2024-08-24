b318902ab409a7e33b8a489c7a3448d821b6ea50
afeefaKhaliq <afeefakhaliq045@gmail.com>
2024-08-24 12:28:17 -0700

    petInfo

--- client/src/Components/Landing/Landing.js
+++ client/src/Components/Landing/Landing.js
@@ -1,20 +1,23 @@
 // src/components/LandingPage/LandingPage.js
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React , {useState, useEffect} from 'react';
 import './Landing.css';
-import petImage from './LandingPage.jpg'; // Make sure the image path is correct
+//import petImage from './LandingPage.jpg'; // Make sure the image path is correct
+import petImage from '../../Assets/Landing/imagess.avif'
 
 const Landing = () => {
-  const navigate = useNavigate();
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setLoaded(true), 300); // Delay to ensure content is ready
+  }, []);
 
   return (
-    <div className="landing-container">
-      <h1>Welcome to Pet Paradise</h1>
-      <p>Your one-stop destination to find and adopt your perfect pet!</p>
-      <img src={petImage} alt="Pets" />
-      <div className="button-group">
-        <button onClick={() => navigate('/login')}>Login</button>
-        <button onClick={() => navigate('/signup')}>Sign Up</button>
+    <div className={`home-container ${loaded ? 'loaded' : ''}`}>
+      <img src={petImage} alt="Background" className="background-image" />
+      <div className="overlay">
+        <h1>Welcome to pet Paradise</h1>
+        <p>Once you have decided to get a pet and have an idea of the type of pet you are looking for, the next step is often “Where do I find my pet? This is your one-stop destination to find and adopt your perfect pet!</p>
+        
       </div>
     </div>
   );
